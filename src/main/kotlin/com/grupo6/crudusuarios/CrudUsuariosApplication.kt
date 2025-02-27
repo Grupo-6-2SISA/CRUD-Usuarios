7dc0032079c0608fc93842a67df33b31f15eaff7
Marcio Juliao <marcio.juliao@sptech.school>
2025-02-26 23:35:17 -0300

    Commit Inicial

---
+++ src/main/kotlin/com/grupo6/crudusuarios/CrudUsuariosApplication.kt
@@ -0,0 +1,11 @@
+package com.grupo6.crudusuarios
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudUsuariosApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrudUsuariosApplication>(*args)
+}
---
+++ src/test/kotlin/com/grupo6/crudusuarios/CrudUsuariosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.grupo6.crudusuarios
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudUsuariosApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
