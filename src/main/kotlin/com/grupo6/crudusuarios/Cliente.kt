834b2ea7ca99d2566fec3442e40559c0db6858bd
PedroSouzaSptech <pedro.bsouza@sptech.school>
2025-03-02 11:58:28 -0300

    chore: adicionando dependência Jackson, pois o Spring Boot a utiliza

---
+++ src/main/kotlin/com/grupo6/crudusuarios/Cliente.kt
@@ -0,0 +1,7 @@
+package com.grupo6.crudusuarios
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import java.time.LocalDate
+
+open class Cliente {
+}
---
+++ src/main/kotlin/com/grupo6/crudusuarios/ClienteController.kt
@@ -0,0 +1,20 @@
+package com.grupo6.crudusuarios
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/cliente")
+class ClienteController {
+
+
+}
