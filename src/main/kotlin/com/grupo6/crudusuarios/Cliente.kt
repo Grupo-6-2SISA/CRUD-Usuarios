1a83d2eabd43acd25958e38a0adce427a17a5f3d
PedroSouzaSptech <pedro.bsouza@sptech.school>
2025-03-04 15:35:34 -0300

    feat: adicionando funcionalidade de cadastrar e remover clientes

--- src/main/kotlin/com/grupo6/crudusuarios/Cliente.kt
+++ src/main/kotlin/com/grupo6/crudusuarios/Cliente.kt
@@ -3,5 +3,16 @@ package com.grupo6.crudusuarios
 import com.fasterxml.jackson.annotation.JsonIgnore
 import java.time.LocalDate
 
-open class Cliente {
+data class Cliente (
+    var nome: String? = null,
+    var cpf: String? = null,
+    var telefone: Int? = null,
+    var email: String? = null,
+    var sexo: String? = null,
+    var rg: String? = null,
+    var causa: String? = null,
+    var dtNasc: LocalDate? = null,
+    var proAbono: Boolean? = null
+){
+
 }
--- src/main/kotlin/com/grupo6/crudusuarios/ClienteController.kt
+++ src/main/kotlin/com/grupo6/crudusuarios/ClienteController.kt
@@ -11,10 +11,40 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/cliente")
 class ClienteController {
 
+    val clientes = mutableListOf<Cliente>()
 
-}
+    @PostMapping
+    fun criar(@RequestBody novoCliente: Cliente): ResponseEntity<Any> {
+        return if (novoCliente.nome != null &&
+            novoCliente.cpf != null &&
+            novoCliente.rg != null &&
+            novoCliente.causa != null &&
+            novoCliente.sexo != null &&
+            novoCliente.telefone != null &&
+            novoCliente.email != null &&
+            novoCliente.proAbono != null &&
+            novoCliente.dtNasc != null
+        ) {
+            clientes.add(novoCliente)
+            ResponseEntity.status(201).body(novoCliente)
+        } else {
+            ResponseEntity.status(400).body(mapOf("error" to "Dados inválidos ou incompletos"))
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun excluir(@PathVariable id: Int): ResponseEntity<String> {
+        return if (id in clientes.indices) {
+            clientes.removeAt(id)
+            ResponseEntity.ok("Cliente excluído com sucesso")
+        } else {
+            ResponseEntity.status(404).body("Cliente não encontrado")
+        }
+
+    }}
