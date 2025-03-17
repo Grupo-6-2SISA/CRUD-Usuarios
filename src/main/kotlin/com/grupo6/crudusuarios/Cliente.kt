9f55e3029a9de92f9d6206bb88eeb1a40369cda8
Marcio Juliao <marcio.juliao@sptech.school>
2025-03-17 17:24:31 -0300

    feat(Cliente): Adequa classe para modelagem de BD

--- src/main/kotlin/com/grupo6/crudusuarios/Cliente.kt
+++ src/main/kotlin/com/grupo6/crudusuarios/Cliente.kt
@@ -5,12 +5,18 @@ import java.time.LocalDate
 
 data class Cliente (
     var nome: String,
+    var email: String,
+    var telefone: String,
     var cpf: String? = null,
     var cnpj: String? = null,
-    var telefone: String,
-    var email: String,
     var rg: String? = null,
-    var causa: String? = null,
-    var dtNasc: LocalDate? = null,
-    var proBono: Boolean? = null
+    var dataNascimento: LocalDate? = null,
+    var dataInicio: LocalDate = LocalDate.now(),
+    var enderecoCompleto: String? = null,
+    var cep: String? = null,
+    var descricao: String? = null,
+    var inscricaoEstadual: String? = null,
+    var isProBono: Boolean = false,
+    var isAtivo: Boolean = true,
+    var isJuridico: Boolean = false
 )
